fcdeb213f45c10debf3bb4ba56208d5a706eb17d
Yancey_Chan <chenyuanshan@outlook1120.com>
2017-09-22 16:12:59 +0800

    no message

--- routes/payuat-pweb.js
+++ routes/payuat-pweb.js
@@ -2,8 +2,6 @@ let Mock = require('mockjs');
 let express = require('express');
 let router = express.Router();
 
-//http://127.0.0.1:3000/cweb/app/home
-
 router.all('/QueryGiveUpReason.do', (req, res) => {
     res.json(Mock.mock({
         "_RejCode": "000000",
@@ -16,7 +14,7 @@ router.all('/QueryGiveUpReason.do', (req, res) => {
 
 router.all('/BankBindListQuery.do', (req, res) => {
     res.json(Mock.mock({
-        "List|1-6": [{
+        "List|0-6": [{
                 "Instname": "银联",
                 "SubItems": [{
                         "Loanaccount": "70100030001324423",
@@ -38,12 +36,12 @@ router.all('/BankBindListQuery.do', (req, res) => {
                 },
                 "Bankcardkind": "0",
                 "Daymax": "300000",
-                "loanList": [{
+                "loanList|0-3": [{
                     "ClosPenRate": "0",
                     "MonFeeRate3": "0",
                     "MonFeeRate2": "0",
-                    "OpenBank": "102",
-                    "ProdType": "1022",
+                    "OpenBank": /10[1-9]/,
+                    "ProdType": /(1022|1012|2113)/,//花贷购代码
                     "MonFeeRate1": "0",
                     "DebitCardNo": "1994959645188488189",
                     "CurrRepayFg": "1",
@@ -59,13 +57,13 @@ router.all('/BankBindListQuery.do', (req, res) => {
                     "LoanStatus": "N",
                     "MonRate": "1.5",
                     "AccountStatus": "LOAN",
-                    "RepayData": "\u0000351-07-24",
+                    "RepayData": "@date()",
                     "DailyRate": "0.05"
                 }],
                 "Instcode": "CUP",
                 "Defaultbank": "1",
                 "Singlemin": "0",
-                "Bankcode": "@natural( 101, 109 )",
+                "Bankcode": /10[1-9]/,
                 "Daymin": "0",
                 "Idtype": "2",
                 "Idno": "44018419900324391X",
@@ -75,6 +73,41 @@ router.all('/BankBindListQuery.do', (req, res) => {
                 "Binddate": "@date(yyyyMMdd)"
             }],
         "_RejCode": "000000"
+    }));
+});
+
+router.all('/MerHotPrdDetailQry.do', (req, res) => {
+    res.json(Mock.mock({
+        "PrdTotalPrice" : "@integer(100,10000)",
+        "MerChantSeq" : "106",
+        "PrdInventory" : "@natural( 0, 200 )",
+        "_RejCode" : "000000",
+        "PrdShowImgUrlList" : [
+          "@image('300x500',@color,'ddd')",
+          "@image('300x500',@color)"
+        ],
+        "PrdImgUrlList" : [
+          "@image('300x250',@color,'ddd')",
+          "@image('300x250',@color,'ddd')",
+          "@image('300x250',@color,'ddd')",
+          "@image('300x250',@color,'ddd')"
+        ],
+        "PeriodScope" : "6,12",
+        "PrdNum" : "fefef",
+        "Promise" : "%25E6%2597%25A0",
+        "PrdEndDate" : "2018-02-01 00:00:00",
+        "PrdBigType" : 3,
+        "PrdName" : "@ctitle(4,15)",
+        "PrdParameter" : "玫瑰金",
+        "PrdCreateDate" : "2017-04-28 14:44:30",
+        "ProdCode" : "21130191",
+        "PrdBeginDate" : "2017-04-28 00:00:00",
+        "PrdNewPrice" : "@integer(100,10000)",
+        "TwoDimensionCode" : "http:\/\/20.223.0.37\/img\/home\/csii\/advforupload\/HOTCODE_20170428_7C5F725A9B90497C8EED50466763464E.jpg",
+        "PrdDesc" : "@cparagraph(4,6)",
+        "LoanPurpose" : "3",
+        "PrdId" : 847,
+        "PrdSmallType" : 4
     }));
 });
 
